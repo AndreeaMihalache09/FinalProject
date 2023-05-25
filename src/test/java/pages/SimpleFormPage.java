e8e241ae75ef32442d2bb9458c6f9b5f05e6587d
Firstname Lastname <andreea.mihalache09@gmail.com>
2023-05-25 21:39:20 +0300

    Commit 2

---
+++ src/test/java/pages/SimpleFormPage.java
@@ -0,0 +1,44 @@
+package pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+public class SimpleFormPage extends BasePage{
+    /*super can be used to refer immediate parent class instance variable.
+   super can be used to invoke immediate parent class method.
+   super() can be used to invoke immediate parent class constructor.*/
+    public SimpleFormPage(WebDriver driver){
+        super(driver);
+    }
+
+    //Locators for Simple Input Field
+    By TextField = By.id("user-message");
+    public void selectTextField(String arg1) {
+        driver.findElement(TextField).sendKeys(arg1);
+    }
+    By GetCheckedValue = By.id("showInput");
+    public void clickOnGetCheckedValue(){ driver.findElement(GetCheckedValue).click();}
+    By YourMessageResult = By.id("message");
+
+
+    //Locators for Two Input Fields
+    By TwoInputFieldsEnterA = By.id("sum1");
+    public void selectTwoFieldsEnterA(String arg1) {
+        driver.findElement(TwoInputFieldsEnterA).sendKeys(arg1);
+    }
+
+    By TwoInputFieldsEnterB = By.id("sum2");
+    public void selectTwoFieldsEnterB(String arg1) {
+        driver.findElement(TwoInputFieldsEnterB).sendKeys(arg1);
+    }
+    By GetValuesButton = By.xpath("//*[@id=\"gettotal\"]/button");
+    public void clickGetValuesButton(){ driver.findElement(GetValuesButton).click();}
+
+
+
+    //Method to click on Single and Two input fields
+
+
+
+
+
+
+}
